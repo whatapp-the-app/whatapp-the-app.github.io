6810de451e6785d9a9c2b2b944ab9261a516f006
kartonixon <adamkartanowicz@gmail.com>
2020-04-04 22:47:42 +0200

    question update

--- what_app/src/components/Question.js
+++ what_app/src/components/Question.js
@@ -1,59 +1,128 @@
 import React from 'react';
 import Answer from './Answer';
+import { Button } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
 
 function BreadcrumbButtons(props) {
     let array = [];
-    for(let i = 0; i < props.items.length; i++) {
+    for (let i = 0; i < props.items.length; i++) {
         array.push(
-        <button key={i}>{props.items[i]}</button>
+            <button key={i}>{props.items[i]}</button>
         );
     }
     return (
-    <div>
-        {array}
-    </div>
+        <div>
+            {array}
+        </div>
     );
 }
 
-function Question(props){
-    const [breadcrumb = "START",setBreadcrumb]=React.useState(props.breadcrumb);
-    const [questions, setQuestions]=React.useState(props.questions)
-    const [communicators,setCommunicators]=React.useState(props.communicators)
-    const [currentText,setCurrentText]=React.useState(questions[0].TextField)
-    const [currentQuestion,setCurrentQuestion]=React.useState(questions[0])
-    const breadcrumbs_list = breadcrumb.split(' ');
-    const [hasAnswer,setHasAnswer]= React.useState(false);
+function getAllMaxInMap(map) {
+    let arr = [];
 
-    function applyAnswer(ans){
-        if (ans===true)
-        {let tempCommunicators=[];
-        communicators.map((communicator)=>{
-            if(communicator[currentQuestion.feature]==ans){
-                tempCommunicators.push(communicator);
-            }
-        })
-        setCommunicators(tempCommunicators);}
+    const max = Array.from(map.values()).reduce(
+        (a, b) => a >= b ? a : b
+    );
+
+    console.log(max);
+
+    for (const [key, value] of map) {
+        if (value === max) {
+            arr.push(key);
+        }
     }
 
-    function checkIfHasAnswer(ans){
-        
-        if(questions.length==1 || communicators.length ==1){
-            localStorage.setItem("communicators",{communicators});
+    return arr;
+}
+
+function Question(props) {
+    const [breadcrumb = "START", setBreadcrumb] = React.useState(props.breadcrumb);
+    const breadcrumbs_list = breadcrumb.split(' ');
+
+    const [questions, setQuestions] = React.useState(props.questions);
+    const [currentQuestion, setCurrentQuestion] = React.useState(questions[0]);
+
+    const [communicators, setCommunicators] = React.useState(props.communicators);
+    const [communicatorScores, setCommunicatorScores] = React.useState(
+        new Map(communicators.map(obj => [obj, 0]))
+    );
+    const [bestFitCommunicators, setBestFitCommunicators] = React.useState(null);
+
+    const [currentText, setCurrentText] = React.useState(questions[0].TextField);
+    const [hasAnswer, setHasAnswer] = React.useState(false);
+
+    function applyAnswer(ans) {
+        let tempCommunicatorScores = new Map();
+
+        for (const [key, value] of communicatorScores) {
+            tempCommunicatorScores.set(key, key[currentQuestion.feature] === true ? value + ans : value);
+        }
+
+        setCommunicatorScores(tempCommunicatorScores);
+    }
+
+    function checkIfHasAnswer(ans) {
+
+        if (questions.length == 1 || communicators.length == 1) {
+
+            setBestFitCommunicators(getAllMaxInMap(communicatorScores));
             setHasAnswer(true);
-            localStorage.setItem("hasAnswer",true);
-        }else{
+
+            // localStorage.setItem("communicators", {communicators});
+            // localStorage.setItem("hasAnswer", true);
+        } else {
             applyAnswer(ans);
             setCurrentQuestion(questions.pop());
         }
+
+
         //TODO case when all communicators would be rejected
     }
-    return(
-        <div>{hasAnswer? <Answer communicators={communicators}/>:<div><ul><BreadcrumbButtons items={breadcrumbs_list} /></ul>
-        <p>{currentQuestion.TextField}</p>
-        <button onClick={()=>{checkIfHasAnswer(true);setBreadcrumb(breadcrumb + " yes")}}>yes</button>
-        <button onClick={()=>{checkIfHasAnswer(false);setBreadcrumb(breadcrumb + " no")}}>no</button></div>}
-            
-        </div>
+
+    const strongButtonText = "stronk";
+    const okButtonText = "yass";
+    const noButtonText = "bruh";
+
+    const classes = useStyles();
+
+    return (
+        <Grid item xs={12}>
+          <div className={classes.paper}>
+
+            {hasAnswer ? <Answer communicators={bestFitCommunicators}/> : <div>
+                <BreadcrumbButtons items={breadcrumbs_list}/>
+                <p>{currentQuestion.TextField}</p>
+
+                <Button variant="contained" color="primary" onClick={() => {
+                    checkIfHasAnswer(0);
+                    setBreadcrumb(breadcrumb + " " + noButtonText)
+                }}>{noButtonText}
+                </Button>
+
+                <Button variant="contained" color="primary" onClick={() => {
+                    checkIfHasAnswer(1);
+                    setBreadcrumb(breadcrumb + " " + okButtonText)
+                }}>{okButtonText}
+                </Button>
+
+                <Button variant="contained" color="primary" onClick={() => {
+                    checkIfHasAnswer(2);
+                    setBreadcrumb(breadcrumb + " " + strongButtonText)
+                }}>{strongButtonText}
+                </Button>
+            </div>}
+                
+            </div>
+        </Grid>
     )
 }
 
