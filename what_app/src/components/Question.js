6bfcdc368fb510777d8249f0aa6cf687360c9601
kartonixon <adamkartanowicz@gmail.com>
2020-04-04 14:52:21 +0200

    added breadcrumbs buttons

--- what_app/src/components/Question.js
+++ what_app/src/components/Question.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
+function BreadcrumbList(props) {
+    let array = [];
+    for(let i = 0; i < props.items.length; i++) {
+      array.push(
+        <button key={i}>{props.items[i]}</button>
+      );
+    }
+    return (
+      <div>
+        {array}
+      </div>
+    );
+}
+
 function Question(props){
-    const [breadcrumb,setBreadcrumb]=React.useState(props.breadcrumb);
+    const [breadcrumb = "START",setBreadcrumb]=React.useState(props.breadcrumb);
     const [question,setQuestion]=React.useState(props.question)
+
+    const breadcrumbs_list = breadcrumb.split(' ');
 
     return(
         <div>
-            <p>breadcrumb</p>
+            <ul><BreadcrumbList items={breadcrumbs_list} /></ul>
             <p>Lorem ipsum</p>
-            <button >yes</button>
-            <button >no</button>
+            <button onClick={()=>setBreadcrumb(breadcrumb + " yes")}>yes</button>
+            <button onClick={()=>setBreadcrumb(breadcrumb + " no")}>no</button>
         </div>
     )
 }
